305d263aba9881a258fd5001c4f1a5bd7dffb92e
zhangxiaoqiang <xqzh@ideabinder.com>
2017-03-13 19:31:05 +0800

    验证手机号

--- resumeby/src/App.vue
+++ resumeby/src/App.vue
@@ -68,15 +68,37 @@
               <span>zhangxiaoqiangjoe@gmail.com</span>
             <el-col>
           </el-row>
-
         </div>
 
         <div class="grid-content bg-purple">
+          <p style="margin-top:62px;color:#FFA500">2015-10—现在</p>
+          <el-tag type="warning">北京班德尔科技有限公司</el-tag>
+          <el-tag type="success">iOS开发</el-tag>
+          <el-card class="box-card">
+            <p><font color=#FFA500>|</font> 构建帮助医药企业精准定位目标医生的移动化SFA管理工具</p>
+            <p><font color=#FFA500>|</font> 杨森LOC销售统计图表模块 Vue+ECharts重构</p>
+            <p><font color=#FFA500>|</font> 发起并负责Jenkins+蒲公英自动化构建公司iOS项目</p>
+            <p><font color=#FFA500>|</font> 使用蒲公英API，设计完成公司内部应用下载平台IDB Store</p>
+            <p><font color=#FFA500>|</font> 葛兰素史克（以下简称GSK）HTTP/1.1后，完成与后台的HTTPS双向认证</p>
+            <p><font color=#FFA500>|</font> 针对GSK新增安全测试，将GSK的项目作代码混淆处理，增加反编译难度</p>
+            <p><font color=#FFA500>|</font> 重写公司所有项目使用的视频课程播放器，使用AVPlayer，支持画中画</p>
+            <p><font color=#FFA500>|</font> 百度地图后台持续定位失败后的解决方案</p>
+            <p><font color=#FFA500>|</font> 为Synapse平台设计完成公司内部直播推流测试工具</p>
+            <p><font color=#FFA500>|</font> Novo Nordisk（以下简称NN）所有大区APP升级64位</p>
+            <p><font color=#FFA500>|</font> NN 美洲区APP国际化功能更新</p>
+            <p><font color=#FFA500>|</font> SQLite数据库迁移</p>
+            <p><font color=#FFA500>|</font> 使用SQLCipher对所有项目本地数据库加密</p>
+            <p><font color=#FFA500>|</font> GSK、NN、杨森的产品升级和维护</p>
+            <p><font color=#FFA500>|</font> 肿么办 V1.0开发</p>
+          </el-card>
+
+        </div>
+        <!-- <div class="grid-content bg-purple">
           <p style="margin-top:10px;color:#FFA500">2014-10~2015-07</p>
           <el-tag type="warning">青岛联创优内科技有限公司</el-tag>
           <el-tag type="primary">.NET开发</el-tag>
           <el-tag type="success">iOS开发</el-tag>
-        </div>
+        </div> -->
 
       </el-col>
     </el-row>
@@ -111,6 +133,15 @@ export default {
       cxt.stroke();
 
       cxt.moveTo(7.5,23);
+      cxt.lineTo(7.5,146);
+      cxt.stroke();
+
+      cxt.beginPath();
+      cxt.arc(7,150,5,0,Math.PI*2,true);
+      cxt.closePath();
+      cxt.stroke();
+
+      cxt.moveTo(7.5,154);
       cxt.lineTo(7.5,496);
       cxt.stroke();
 
@@ -144,6 +175,11 @@ export default {
 
 .icon_person {
 
+}
+
+.box-card {
+  width: 100%;
+  margin-top: 10px;
 }
 
 h1, h2 {
